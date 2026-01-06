445a322714ddee5394fea395312d872e124104df
varma1225 <varma08.asa@gmail.com>
2026-01-06 18:41:08 +0530

    Delete db.py

--- db.py
+++
@@ -1,5 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb+srv://varma:1225@varma.f5zdh.mongodb.net/?appName=varma")
-db = client["voice_authentication"]
-collection = db["voice_authentication"]
